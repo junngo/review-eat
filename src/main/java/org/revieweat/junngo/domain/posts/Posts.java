9c0e07240ea266621c198f1c1b13eb3c3d1b9f3c
junngo <myeongjun.ko@gmail.com>
2020-11-20 15:23:24 +0900

    added BaseTimeEntity

--- src/main/java/org/revieweat/junngo/domain/posts/Posts.java
+++ src/main/java/org/revieweat/junngo/domain/posts/Posts.java
@@ -3,13 +3,14 @@ package org.revieweat.junngo.domain.posts;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.revieweat.junngo.domain.BaseTimeEntity;
 
 import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
